20e7265a3fae7da8347f946583e95a98443b4e88
NguyePham <nguyenminhpham24@gmail.com>
2024-05-05 16:09:45 +0700

    Add GPS button

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,7 +1,8 @@
 import * as React from "react";
-import * as Location from "expo-location"
-import * as TaskManager from 'expo-task-manager'
-import {Dimensions, StyleSheet, View} from "react-native";
+import * as Location from "expo-location";
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from 'expo-background-fetch';
+import {Dimensions, Text, StyleSheet, View, TouchableOpacity} from "react-native";
 import BottomButtonBar from "../components/NavigatorBottomBar";
 import Landmark from "../components/Landmark";
 import MapView, {Polygon, Polyline, PROVIDER_GOOGLE} from 'react-native-maps';
@@ -9,13 +10,53 @@ import mapData from "../../data/mapData.json";
 import mapStyle from "../../data/mapStyle.json";
 
 const REFRESH_LOCATION_PERMISSION = 'location-permission';
+const INTERVAL = 5;
 let foregroundSubscription  = null;
 
-TaskManager.defineTask(REFRESH_LOCATION_PERMISSION, async () => {
+// let setLocationPermission = (state) => {
+//   console.log(state.toString() + 'has not initialized');
+// }
 
-});
+// const toggleLocationPermission = async () => {
+//   try {
+//     console.log('Task executed!');
+//     const {status} = await Location.getForegroundPermissionsAsync();
+//     if (status ==='granted') {
+//       setLocationPermission(true);
+//     } else {
+//       setLocationPermission(false);
+//     }
+//     return (status === 'granted') ? BackgroundFetch.BackgroundFetchResult.NewData : BackgroundFetch.BackgroundFetchResult.NoData;
+//   } catch (error) {
+//     console.log(error.message);
+//     return BackgroundFetch.BackgroundFetchResult.Failed;
+//   }
+// }
+// TaskManager.defineTask(REFRESH_LOCATION_PERMISSION, toggleLocationPermission);
 
-TaskManager.getRegisteredTasksAsync().then(r => console.log(r));
+// TaskManager.getRegisteredTasksAsync().then(r => console.log(r));
+//
+// const registerLocationPermissionRefresh = async () => {
+//   try {
+//     await BackgroundFetch.registerTaskAsync(REFRESH_LOCATION_PERMISSION, {
+//       minimumInterval: INTERVAL,
+//       startOnBoot: true,
+//       stopOnTerminate: true,
+//     }).then(r => console.log('Task registered!'));
+//   } catch (err) {
+//     console.log('Task register failed: ', err);
+//   }
+// }
+
+const GPSButton = ({title, onPress}) => {
+  return (
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text>{title}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 export default function MapScreen() {
   const [mapRegion, setMapRegion] = React.useState({
@@ -25,11 +66,10 @@ export default function MapScreen() {
   });
 
   const [location, setLocation] = React.useState(null);
-  const [showsUserLocation, setShowsUserLocation] = React.useState(true);
+  const [showsUserLocation, setShowsUserLocation] = React.useState(false);
+  // setLocationPermission = setShowsUserLocation;
 
   const startForegroundUpdate = async () => {
-    // Make sure that foreground location tracking is not running
-    foregroundSubscription?.remove();
 
     try {
       // Start watching position in real-time
@@ -47,78 +87,110 @@ export default function MapScreen() {
     }
   }
 
+  const onGPSButtonPress = async () => {
+    try {
+      console.log('Task executed!');
+      const {granted} = await Location.getForegroundPermissionsAsync();
+      console.log('Granted: ' + granted);
+      console.log('showUserLocation: ' + showsUserLocation);
+
+      if (granted && !showsUserLocation) {
+        console.log('Location permission granted.');
+        await startForegroundUpdate();
+        setShowsUserLocation(true);
+      } else if (showsUserLocation) {
+        console.log('Location access turned off.');
+        setShowsUserLocation(false);
+      }
+      return (granted) ? BackgroundFetch.BackgroundFetchResult.NewData : BackgroundFetch.BackgroundFetchResult.NoData;
+    } catch (error) {
+      console.log(error.message);
+      return BackgroundFetch.BackgroundFetchResult.Failed;
+    }
+  }
+
   React.useEffect(() => {
     (async () => {
-      let {status} = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        alert('Failed to request location foreground permission');
-        return;
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Failed to request location foreground permission');
+          return;
+        }
+        // await registerLocationPermissionRefresh();
+        await startForegroundUpdate();
+        setShowsUserLocation(true);
+      } catch (error) {
+        console.log("Error requesting location permission:", error.message);
+        // Handle error (e.g., show error message to the user)
       }
-      await startForegroundUpdate();
     })();
   }, []);
 
   const lastRegion = React.useRef(mapRegion);
   const lastPosition = React.useRef(location);
-
   let landmarkNumGen = 0;
+
   return (
-    <View style={styles.container}>
-      <View style={styles.mapField}>
-        <MapView style={styles.map}
-                 provider={PROVIDER_GOOGLE}
-                 showsUserLocation={showsUserLocation}
-                 followsUserLocation={true}
-                 customMapStyle={mapStyle}
-                 initialRegion={mapRegion}
-                 onRegionChange={(region) => {
-                   lastRegion.current = region;
-                   setTimeout(() => {
-                     if (lastRegion.current === region)
-                       setMapRegion(region)
-                   }, 100);
-                 }}
-                 onUserLocationChange={(position) => {
-                   lastPosition.current = position;
-                   setTimeout(() => {
-                     if (lastPosition.current === position)
-                       setLocation(position)
-                   }, 100);
-                 }}>
-          <Polygon
-            coordinates={mapData.citadelRegion}
-            strokeWidth={2}
-            strokeColor="orange">
-          </Polygon>
-          {
-            mapData.roads.map((road, index) => <Polyline
-              key={index}
-              coordinates={road.map(id => mapData.roadIntersections.find((x) => x.id === id))}
-              strokeWidth={0.04 / lastRegion.current.latitudeDelta}
-              strokeColor="white"
-            ></Polyline>)
-          }
-          {
-            mapData.landmarks.map((landmark) =>
-              <Landmark
-                key={landmark.id}
-                {...landmark}
-                number={landmark?.marker?.number || ++landmarkNumGen}/>)
-          }
-          {
-            mapData.campuses.map((campus, index) => <Polygon
-              key={index}
-              zIndex={-3}
-              coordinates={campus}
-              strokeWidth={1}
-              strokeColor="grey"
-              fillColor="rgb(240,240,200)">
-            </Polygon>)
-          }
-        </MapView>
+    <>
+      <View style={styles.container}>
+        <View style={styles.mapField}>
+          <MapView style={styles.map}
+                   provider={PROVIDER_GOOGLE}
+                   showsUserLocation={showsUserLocation}
+                   followsUserLocation={true}
+                   customMapStyle={mapStyle}
+                   initialRegion={mapRegion}
+                   onRegionChange={(region) => {
+                     lastRegion.current = region;
+                     setTimeout(() => {
+                       if (lastRegion.current === region)
+                         setMapRegion(region)
+                     }, 100);
+                   }}
+                   onUserLocationChange={(position) => {
+                     lastPosition.current = position;
+                     setTimeout(() => {
+                       if (lastPosition.current === position)
+                         setLocation(position)
+                     }, 100);
+                   }}>
+            <Polygon
+              coordinates={mapData.citadelRegion}
+              strokeWidth={2}
+              strokeColor="orange">
+            </Polygon>
+            {
+              mapData.roads.map((road, index) => <Polyline
+                key={index}
+                coordinates={road.map(id => mapData.roadIntersections.find((x) => x.id === id))}
+                strokeWidth={0.04 / lastRegion.current.latitudeDelta}
+                strokeColor="white"
+              ></Polyline>)
+            }
+            {
+              mapData.landmarks.map((landmark) =>
+                <Landmark
+                  key={landmark.id}
+                  {...landmark}
+                  number={landmark?.marker?.number || ++landmarkNumGen}/>)
+            }
+            {
+              mapData.campuses.map((campus, index) => <Polygon
+                key={index}
+                zIndex={-3}
+                coordinates={campus}
+                strokeWidth={1}
+                strokeColor="grey"
+                fillColor="rgb(240,240,200)">
+              </Polygon>)
+            }
+          </MapView>
+        </View>
+        <BottomButtonBar/>
       </View>
-      <BottomButtonBar/>
-    </View>
+      <GPSButton title={'GPS'} onPress={onGPSButtonPress}/>
+    </>
   );
 }
 
@@ -144,5 +216,14 @@ const styles = StyleSheet.create({
     color: "black",
     fontSize: 20,
     fontWeight: "bold"
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 80, // Adjust this value to change vertical position
+    right: 5, // Adjust this value to change horizontal position
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+    elevation: 2,
   },
 });
