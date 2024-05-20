096659595d9bb78f75b8ddd3bc143d62935d08d4
ChuTu15 <21020393@vnu.edu.vn>
2024-05-20 21:39:30 +0700

    chore: Update Java build configuration and Android dependencies

--- android/app/src/release/java/com/hoangthanhthanglong/exporeactnative/ReactNativeFlipper.java
+++ android/app/src/release/java/com/hoangthanhthanglong/exporeactnative/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.hoangthanhthanglong;
+package com.HoangThanhThangLong.exporeactnative;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
